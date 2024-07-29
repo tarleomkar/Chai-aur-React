cc64e34ee0545563b8f8d861f15189f7cba92410
Omkar Yogesh Tarle <tarleomkar8@gmail.com>
2024-07-30 00:16:52 +0530

    Added new folders and files

---
+++ 04_My Props Gyan/props_learning/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="John" />
+      <Greeting name="Jane" />
+    </div>
+  );
+}
+
+export default App;
