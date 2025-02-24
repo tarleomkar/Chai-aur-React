7725a3ae79f6cc62e0213b694b31cf41b46c9c53
Omkar Tarle <tarleomkar8@gmail.com>
2025-02-24 15:07:38 +0530

    functionality done

--- apihandling/frontend/src/App.jsx
+++ apihandling/frontend/src/App.jsx
@@ -4,7 +4,29 @@ import axios from "axios"
 
 const App = () => {
 
-  const [products, error, loading] = customReactQuery('/api/products')
+  // custom query or hook
+  // const [products, error, loading] = customReactQuery('/api/products')
+
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [search, serSearch] = useState('')
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true)
+        setError(false)
+        const response = await axios.get('/api/products?search=' + search)
+        console.log(response.data);    
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+      }
+    })()
+
+  }, [search])
 
   if (error) {
     return <h1>Something went wrong</h1>
@@ -17,6 +39,14 @@ const App = () => {
   return (
     <>
     <h1>Chai aur API/Axios in react</h1>
+    <input type="text" placeholder="Search"
+    value={search}
+    onChange={(e) => serSearch(e.target.value)}
+    />
+
+    {loading && (<h1>Loading...</h1>)}
+    {error && (<h1>Something went wrong</h1>)}
+    
     <h2>Number of Products are: {products.length}</h2>
     </>
   )
@@ -25,25 +55,6 @@ const App = () => {
 export default App
 
 const customReactQuery = (urlPath) => {
-  const [products, setProducts] = useState([])
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true)
-        setError(false)
-        const response = await axios.get(urlPath)
-        console.log(response.data);    
-        setProducts(response.data)
-        setLoading(false)
-      } catch (error) {
-        setError(true)
-      }
-    })()
-
-  }, [])
 
   return [products, error, loading]
 }
