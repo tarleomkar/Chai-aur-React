45bd599cb32b8b78a8adbff6e372f766cd15b840
Omkar Tarle <tarleomkar8@gmail.com>
2025-03-17 15:34:52 +0530

    added add to cart functionality

--- shopping cart/src/App.jsx
+++ shopping cart/src/App.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
 import NavBar from './components/NavBar'
 import Shop from './components/Shop'
+import './App.css'
 
 const App = () => {
   const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false)
 
   const handleClick = (item) => {
-    console.log(item);
+    if(cart.some((product) => product.id === item.id)) {
+      setWarning(true);
+      setTimeout(() => setWarning(false), 2000)
+      return
+    }
+    setCart((prevCart) => [...prevCart, item])
   }
 
   return (
     <div>
     <NavBar size={cart.length} />
     <Shop handleClick={handleClick} />
+    {warning && 
+    <div className='warning'>
+    Item is already present in your Cart  
+    </div>}
     </div>
   )
 }
