7844b3a1762970dbfec880fb11c48028f190d612
Omkar Tarle <tarleomkar8@gmail.com>
2025-03-17 14:06:27 +0530

    added nav styling

---
+++ shopping cart/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ImageOff, ShoppingCart } from 'lucide-react';
+import '../styles/navbar.css'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="nav_box">
+        <span className="my_shop">
+          Dxo Shop
+        </span>
+        <div className="cart">
+          <span>
+            <ShoppingCart />
+          </span>
+          <span>1</span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
