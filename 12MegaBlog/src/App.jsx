4d27aa40c6c29445436216e913bb56c6ee97b587
Omkar Tarle <tarleomkar8@gmail.com>
2025-03-22 20:48:55 +0530

    added ui functionality and installed the tailwind css

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,9 +1,40 @@
-const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
-  return (
-    <div>A blog app with react</div>
-  )
-}
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import {Header, Footer} from "./components"
+import { Outlet } from "react-router-dom";
 
-export default App
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user: ", error);
+        dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="w-full block">
+      <Header />
+      <main>
+      TODO:  {/* <Outlet /> */}
+      </main>
+      <Footer/>
+    </div>
+  </div> : null;
+};
+
+export default App;
